9f4e76bbbd3f8be0cd36cd21c773b6744601d53f
Alexander Hansen <79235117+hansen9@users.noreply.github.com>
2025-11-23 14:16:01 +0700

    * add PluginOption to vite.config * clean manual disable for dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// Mock localStorage for build time (BEFORE export)
-if (typeof window === 'undefined') {
-  global.localStorage = {
-    getItem: () => null,
-    setItem: () => {},
-    removeItem: () => {},
-    clear: () => {},
-    key: () => null,
-    length: 0,
-  }
-}
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  define: {
-    __VUE_PROD_DEVTOOLS__: false,
-  },
-  plugins: [
-    vue(),
-    command === 'serve' && vueDevTools(), // <-- THE FIX,
-  ],
-  ssr: {
-    noExternal: ['vue-recaptcha-v3'],
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command }) => {
+  const plugins: PluginOption[] = [vue()]
+
+  if (command === 'serve') {
+    plugins.push(vueDevTools())
+  }
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-}))
+  }
+})
